a7d49263fc76235eb10b3d166b3da004d3950fc8
caoqinfeng <caoqinfeng@fshows.com>
2025-04-16 11:52:17 +0800

    feat(components): 添加 SvgIcon 组件和基础应用结构

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import SvgIcon from './components/SvgIcon.vue'
+</script>
+
+<template>
+  <div class="container">
+    <div class="logo-container">
+      <SvgIcon name="vue" color="#42b883" size="100">
+        <path 
+          d="M24,1.61H14.06L12,5.16,9.94,1.61H0L12,22.39ZM12,14.08,5.16,2.23H9.59L12,6.41l2.41-4.18h4.43Z"
+          fill="currentColor"
+        />
+      </SvgIcon>
+    </div>
+    <HelloWorld msg="SVG 图标库" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import './styles/variables.scss';
+@import './styles/mixins.scss';
+
+.container {
+  @include flex(column, center, center);
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: $spacing-xl;
+}
+
+.logo-container {
+  @include flex(row, center, center);
+  margin: $spacing-xl 0;
+  @include transition(transform, 0.3s, ease);
+  
+  &:hover {
+    transform: scale(1.05);
+  }
+}
+</style>
---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Props {
+  name: string;
+  color?: string;
+  size?: string | number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  color: 'currentColor',
+  size: '24'
+});
+
+const sizeStyle = computed(() => {
+  return typeof props.size === 'number' ? `${props.size}px` : props.size;
+});
+</script>
+
+<template>
+  <svg
+    :width="sizeStyle"
+    :height="sizeStyle"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    :style="{ color: color }"
+    class="svg-icon"
+  >
+    <slot></slot>
+  </svg>
+</template>
+
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
+@import '../styles/mixins.scss';
+
+.svg-icon {
+  display: inline-block;
+  vertical-align: middle;
+  @include transition(transform, 0.2s, ease-in-out);
+  
+  &:hover {
+    opacity: 0.9;
+    transform: scale(1.05);
+  }
+}
+</style> 
