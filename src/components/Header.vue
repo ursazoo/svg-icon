4ebb68a13a1df6a429721c465af5a5f2309522f8
caoqinfeng <caoqinfeng@fshows.com>
2025-04-16 13:27:10 +0800

    feat(components): 添加 Header 组件
    添加了一个新的 Header 组件，具有以下功能：
    1. 显示标题文本
    2. 支持点击事件
    3. 使用 v-model 实现双向绑定

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header">
+        <h1 @click="handleClick">{{ title }}</h1>
+    </div>
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true
+    }
+});
+
+const emit = defineEmits(['update:title']);
+
+const title = ref(props.title);
+
+const handleClick = () => {
+    emit('update:title', 'New Title');
+};
+
+</script>
+
+<style scoped>
+.header {
+    background-color: #f0f0f0;
+}
+</style>
